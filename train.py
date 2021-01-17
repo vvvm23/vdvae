Fixed weightings between kl and reconstruction loss

train.py
@@ -7,6 +7,7 @@ from torchvision.utils import save_image
 
 import numpy as np
 import itertools
+import datetime
 
 from hps import HPS
 from helper import info, error, warning
@@ -20,10 +21,23 @@ else:
 
 def load_dataset(dataset, batch_size):
     if dataset == 'cifar10':
-        # TODO: Normalize input images
-        # TODO: Other transforms too
-        train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=transforms.ToTensor())
-        test_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=transforms.ToTensor())
+        dataset_transforms = transforms.Compose([
+            transforms.RandomRotation(5),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+        train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=dataset_transforms)
+        test_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=dataset_transforms)
+    elif dataset == 'mnist':
+        dataset_transforms = transforms.Compose([
+            transforms.RandomRotation(5),
+            transforms.ToTensor(),
+            # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        ])
+
+        train_dataset = torchvision.datasets.MNIST('data', train=True, download=True, transform=dataset_transforms)
+        test_dataset = torchvision.datasets.MNIST('data', train=False, download=True, transform=dataset_transforms)
     else:
         error(f"Unrecognized dataset '{dataset}'! Exiting..")
         exit()
@@ -34,27 +48,42 @@ def load_dataset(dataset, batch_size):
     # return train_loader, test_loader
     return test_loader, test_loader
 
+def vae_loss(x, model, crit):
+    x = x.to(device)
+    y, decoder_kl = model(x)
+    rl = crit(x, y).mean(dim=(1,2,3))
+    rpp = torch.zeros_like(rl)
+    for k in decoder_kl:
+        rpp += k.sum(dim=(1,2,3))
+    rpp /= np.prod(x.shape[1:])
+    elbo = (rpp + rl*100).mean()
+    return elbo, rl.mean(), rpp.mean()
+
 def train(model, loader, optim, crit, device):
     total_loss, r_loss, kl_loss = 0.0, 0.0, 0.0
     model.train()
     for x, _ in tqdm(loader):
         optim.zero_grad()
-        x = x.to(device)
-
-        y, decoder_kl = model(x)
-        rl = crit(x, y).mean(dim=(1,2,3))
-
-        rpp = torch.zeros_like(rl)
-        for k in decoder_kl:
-            rpp += k.sum(dim=(1,2,3))
-        rpp /= np.prod(x.shape[1:])
-        
-        elbo = (rpp*0.25 + rl).mean()
+        elbo, rl, kl = vae_loss(x, model, crit)
         elbo.backward()
         optim.step()
 
-        rl = rl.mean()
-        kl = rpp.mean()
+        # x = x.to(device)
+
+        # y, decoder_kl = model(x)
+        # rl = crit(x, y).mean(dim=(1,2,3))
+
+        # rpp = torch.zeros_like(rl)
+        # for k in decoder_kl:
+            # rpp += k.sum(dim=(1,2,3))
+        # rpp /= np.prod(x.shape[1:])
+        
+        # elbo = (rpp + rl*100).mean()
+        # elbo.backward()
+        # optim.step()
+
+        # rl = rl.mean()
+        # kl = rpp.mean()
 
         total_loss += elbo
         r_loss += rl
@@ -67,19 +96,20 @@ def evaluate(model, loader, optim, crit, device, img_id=None):
         model.eval()
         for i, (x, _) in enumerate(tqdm(loader)):
             optim.zero_grad()
-            x = x.to(device)
+            elbo, rl, kl = vae_loss(x, model, crit)
+            # x = x.to(device)
 
-            y, decoder_kl = model(x)
-            rl = crit(x, y).mean(dim=(1,2,3))
+            # y, decoder_kl = model(x)
+            # rl = crit(x, y).mean(dim=(1,2,3))
 
-            rpp = torch.zeros_like(rl)
-            for k in decoder_kl:
-                rpp += k.sum(dim=(1,2,3))
-            rpp /= np.prod(x.shape[1:])
+            # rpp = torch.zeros_like(rl)
+            # for k in decoder_kl:
+                # rpp += k.sum(dim=(1,2,3))
+            # rpp /= np.prod(x.shape[1:])
             
-            elbo = (rpp*0.25 + rl).mean()
-            rl = rl.mean()
-            kl = rpp.mean()
+            # elbo = (rpp + rl*100).mean()
+            # rl = rl.mean()
+            # kl = rpp.mean()
 
             total_loss += elbo
             r_loss += rl
@@ -101,19 +131,23 @@ if __name__ == "__main__":
     optim = torch.optim.Adam(model.parameters(), lr=HPS.lr, weight_decay=HPS.decay)
     crit = torch.nn.MSELoss(reduction='none')
 
+    save_id = str(datetime.date.today())
+
     nb_iterations = 0
-    # for ei in range(HPS.nb_epochs):
     for ei in itertools.count():
         train_loss, r_loss, kl_loss = train(model, train_loader, optim, crit, device)
         nb_iterations += len(train_loader)
 
         info(f"training, epoch {ei+1} \t iter: {nb_iterations} \t loss: {train_loss} \t r_loss {r_loss} \t kl_loss {kl_loss}")
         eval_loss, r_loss, kl_loss = evaluate(model, test_loader, optim, crit, device, img_id=ei)
-        info(f"evaluate, epoch {ei+1}/{HPS.nb_epochs} \t iter: {nb_iterations} \t loss: {eval_loss} \t r_loss {r_loss} \t kl_loss {kl_loss}")
+        info(f"evaluate, epoch {ei+1} \t iter: {nb_iterations} \t loss: {eval_loss} \t r_loss {r_loss} \t kl_loss {kl_loss}")
 
         if HPS.checkpoint > 0 and ei > 0 and ei % HPS.checkpoint == 0:
             # TODO: Replace with full checkpointing
-            torch.save(model.state_dict(), f"saved_checkpoints/vdvae-{ei}.pt")
+            torch.save(model.state_dict(), f"saved_checkpoints/{save_id}-vdvae-{ei}.pt")
 
         if nb_iterations > HPS.nb_iterations:
+            info("Maximum iterations reached. Exiting..")
             break
+
+    torch.save(model.state_dict(), f"saved_checkpoints/{save_id}-vdvae-final.pt")